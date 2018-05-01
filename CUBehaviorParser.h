4d229c1b506b5ff2c7656310546002043679b452
Apurv Sethi <as2658@cornell.edu>
2018-05-01 17:22:24 -0400

    Added BehaviorParser to convert json file to BehaviorNodeDef

---
+++ CUBehaviorParser.h
@@ -0,0 +1,88 @@
+//
+//  CUBehaviorParser.h
+//  Cornell University Game Library (CUGL)
+//
+//  This module provides support for a behavior parser in order to create a
+//  BodyDefNode from a JSON.
+//
+//  You should never instantiate an object of this class, as it is only used
+//  for static methods. It has no allocators, initializer, or constructor.
+//
+//  Author: Apurv Sethi
+//  Version: 3/28/2018
+//
+
+#ifndef __CU_BEHAVIOR_PARSER_H__
+#define __CU_BEHAVIOR_PARSER_H__
+
+#include <string>
+#include <vector>
+#include <cugl/ai/behaviorTree/CUBehaviorNode.h>
+#include <cugl/io/CUJsonReader.h>
+
+namespace cugl {
+
+/**
+ * This class provides a behavior node for a behavior tree.
+ *
+ * A behavior node refers to any given node within a beahvior tree,
+ * whether it is a CompositeNode, DecoratorNode, or LeafNode.
+ *
+ * A behavior tree is a construction of behavior nodes, with the top
+ * node that does not have a parent referring to the root of the tree.
+ * The tree must use an update function to run on each tick, updating
+ * the state of each node.
+ *
+ * The root node of a behavior tree returns the state of the currently
+ * running node, or the node that ran during the update cycle.
+ */
+class BehaviorParser {
+public:
+	/**
+	 * Returns a BehaviorNodeDef constructed from the given file.
+	 *
+	 * This function assumes that the file name is a relative path. It will
+	 * search the application assert directory
+	 * {@see Application#getAssetDirectory()} for the file and return false if
+	 * it cannot find it there.
+	 *
+	 * @param file  The relative path to the file.
+	 *
+	 * @return a BehaviorNodeDef constructed from the given file.
+	 */
+	static std::shared_ptr<BehaviorNodeDef> parseFile(const std::string& file) {
+		return parseFile(file.c_str());
+	}
+
+	/**
+	 * Returns a BehaviorNodeDef constructed from the given file.
+	 *
+	 * This function assumes that the file name is a relative path. It will
+	 * search the application assert directory
+	 * {@see Application#getAssetDirectory()} for the file and return false if
+	 * it cannot find it there.
+	 *
+	 * @param file  The relative path to the file.
+	 *
+	 * @return a BehaviorNodeDef constructed from the given file.
+	 */
+	static std::shared_ptr<BehaviorNodeDef> parseFile(const char* file);
+
+private:
+	/**
+	 * Returns a BehaviorNodeDef constructed from the given JsonValue.
+	 *
+	 * This function uses a json in order to construct a BehaviorNodeDef. The
+	 * json must follow certain guidelines required to create a BehaviorNodeDef
+	 * in order to be parsed by the function.
+	 *
+	 * @param json	The JsonValue representing a BehaviorNodeDef.
+	 *
+	 * @return a BehaviorNodeDef constructed from the given JsonValue.
+	 */
+	static std::shared_ptr<BehaviorNodeDef> parseJson(const std::shared_ptr<JsonValue>& json);
+};
+
+
+}
+#endif /* __CU_BEHAVIOR_PARSER_H__ */
---
+++ src/CUBehaviorParser.cpp
@@ -0,0 +1,72 @@
+//
+//  CUBehaviorParser.cpp
+//  Cornell University Game Library (CUGL)
+//
+//  This module provides support for a behavior tree manager.
+//
+//  Author: Apurv Sethi
+//  Version: 3/28/2018
+//
+
+#include <cugl/ai/behaviorTree/CUBehaviorParser.h>
+#include <cugl/util/CUDebug.h>
+#include <unordered_map>
+
+using namespace cugl;
+
+std::unordered_map<std::string, BehaviorNodeDef::Type> typeMap = {
+	{"priority", BehaviorNodeDef::Type::PRIORITY_NODE},
+	{"selector", BehaviorNodeDef::Type::SELECTOR_NODE},
+	{"random", BehaviorNodeDef::Type::RANDOM_NODE},
+	{"inverter", BehaviorNodeDef::Type::INVERTER_NODE},
+	{"timer", BehaviorNodeDef::Type::TIMER_NODE},
+	{"leaf", BehaviorNodeDef::Type::LEAF_NODE}
+};
+
+/**
+ * Returns a BehaviorNodeDef constructed from the given file.
+ *
+ * This function assumes that the file name is a relative path. It will
+ * search the application assert directory
+ * {@see Application#getAssetDirectory()} for the file and return false if
+ * it cannot find it there.
+ *
+ * @param file  The relative path to the file.
+ *
+ * @return a BehaviorNodeDef constructed from the given file.
+ */
+std::shared_ptr<BehaviorNodeDef> BehaviorParser::parseFile(const char* file) {
+	std::shared_ptr<JsonReader> reader = JsonReader::allocWithAsset(file);
+	return parseJson(reader->readJson());
+}
+
+/**
+ * Returns a BehaviorNodeDef constructed from the given JsonValue.
+ *
+ * This function uses a json in order to construct a BehaviorNodeDef. The
+ * json must follow certain guidelines required to create a BehaviorNodeDef
+ * in order to be parsed by the function.
+ *
+ * @param json	The JsonValue representing a BehaviorNodeDef.
+ *
+ * @return a BehaviorNodeDef constructed from the given JsonValue.
+ */
+std::shared_ptr<BehaviorNodeDef> BehaviorParser::parseJson(const std::shared_ptr<JsonValue>& json) {
+	std::shared_ptr<BehaviorNodeDef> node = std::make_shared<BehaviorNodeDef>();
+	node->_name = json->getString("name");
+	std::string type = json->getString("type");
+	CUAssertLog(!type.empty(), "The type of a BehaviorNodeDef must be defined");
+	node->_type = typeMap.at(type);
+
+	node->_preempt = json->getBool("preempt", false);
+	node->_uniformRandom = json->getBool("uniformRandom", true);
+	node->_timeDelay = json->getBool("timeDelay", true);
+	node->_delay = json->getFloat("delay", 1.0f);
+	std::shared_ptr<JsonValue> children = json->get("children");
+	if (children != nullptr) {
+		for (int ii = 0; ii < children->size(); ii++) {
+			node->_children.push_back(parseJson(children->get(ii)));
+		}
+	}
+	return node;
+}
