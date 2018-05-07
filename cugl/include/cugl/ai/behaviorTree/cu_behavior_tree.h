507cf06b772351718b328d36fd0c9945cb8824a9
Andrew Matsumoto <arm297@cornell.edu>
2018-05-07 11:16:34 -0400

    Added reference header for behavior tree module

---
+++ cugl/include/cugl/ai/behaviorTree/cu_behavior_tree.h
@@ -0,0 +1,27 @@
+//
+//  cu_behavior_tree.h
+//  Cornell University Game Library (CUGL)
+//
+//  This header provides access to all of the publicly accessible elements of this package.
+//
+// Author: Andrew Matsumoto
+// Version: 5/7/2018
+//
+#ifndef __CU_BEHAVIOR_TREE_PKG_H__
+#define __CU_BEHAVIOR_TREE_PKG_H__
+
+#include "CUBehaviorAction.h"
+#include "CUBehaviorManager.h"
+#include "CUBehaviorNode.h"
+#include "CUBehaviorParser.h"
+#include "CUBehaviorParser.h"
+#include "CUCompositeNode.h"
+#include "CUDecoratorNode.h"
+#include "CUInverterNode.h"
+#include "CULeafNode.h"
+#include "CUPriorityNode.h"
+#include "CURandomNode.h"
+#include "CUSelectorNode.h"
+#include "CUTimerNode.h"
+
+#endif /* __CU_BEHAVIOR_TREE_PKG_H__ */
